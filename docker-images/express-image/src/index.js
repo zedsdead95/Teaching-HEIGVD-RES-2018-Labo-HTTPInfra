76588a60635a0328e621774a359ef18d6c5e5f20
zedsdead <mr.walid.koubaa@gmail.com>
2018-06-01 09:55:56 +0200

    Added express-image - chance & postman

---
+++ docker-images/express-image/src/index.js
@@ -0,0 +1,44 @@
+var Chance = require('chance');
+var chance = new Chance();
+
+console.log("Bonjour " + chance.name());
+
+var express = require('express');
+var app =express();
+
+app.get('/',function(req,res){
+	res.send(generateStudents());	
+});
+
+app.get('/test',function(req,res){
+	res.send('Hello World !')	
+});
+
+app.listen(3000,function(){
+	console.log("Acception HTTp request on port 3000")
+});
+
+function generateStudents(){
+	var numberOfStudents = chance.Integer({
+		min: 0, max: 10
+	});
+	console.log(numberOfStudents);
+	var students = [];
+	for(var i =0; i< numberOfStudents; i++){
+		var gender = chance.gender();
+		var birthYear = chance.year({
+			min: 1986, max :1996
+		})
+		students.push({
+			firstName: chance.first({
+				gender:gender
+			}),
+			lastname : chance.last(), gender: gender,
+			birthday: chance.birthday({
+				year: birthYear
+			})
+		});
+	};
+	console.log(students);
+	return students;
+}
