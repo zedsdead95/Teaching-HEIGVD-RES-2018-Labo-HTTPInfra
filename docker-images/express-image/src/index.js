33ae423919698beee80e7a83fbea2a2870cacbdc
zedsdead <mr.walid.koubaa@gmail.com>
2018-06-04 11:35:09 +0200

    Docker image for apache reverse proxy

--- docker-images/express-image/src/index.js
+++ docker-images/express-image/src/index.js
@@ -4,34 +4,30 @@ var chance = new Chance();
 console.log("Bonjour " + chance.name());
 
 var express = require('express');
-var app =express();
+var app = express();
 
 app.get('/',function(req,res){
 	res.send(generateStudents());	
 });
 
-app.get('/test',function(req,res){
-	res.send('Hello World !')	
-});
-
 app.listen(3000,function(){
-	console.log("Acception HTTp request on port 3000")
+	console.log("Acception HTTP request on port 3000")
 });
 
 function generateStudents(){
-	var numberOfStudents = chance.Integer({
+	var numberOfStudents = chance.integer({
 		min: 0, max: 10
 	});
 	console.log(numberOfStudents);
 	var students = [];
-	for(var i =0; i< numberOfStudents; i++){
+	for(var i =0; i < numberOfStudents; i++){
 		var gender = chance.gender();
 		var birthYear = chance.year({
 			min: 1986, max :1996
-		})
+		});
 		students.push({
 			firstName: chance.first({
-				gender:gender
+				gender: gender
 			}),
 			lastname : chance.last(), gender: gender,
 			birthday: chance.birthday({
